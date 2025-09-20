7428710b385e1f04f755c8e4bca5bc9f2841ccf7
Wayne Shek <wayne@relentlesstechnology.com>
2025-09-20 15:23:29 -0700

    Init commit

---
+++ src/data/images.js
@@ -0,0 +1,8 @@
+const images = [
+  { id: 1, src: "https://picsum.photos/id/1015/400/300", title: "Mountain" },
+  { id: 2, src: "https://picsum.photos/id/1025/400/300", title: "Dog" },
+  { id: 3, src: "https://picsum.photos/id/1035/400/300", title: "River" },
+  { id: 4, src: "https://picsum.photos/id/1045/400/300", title: "Forest" }
+];
+
+export default images;
